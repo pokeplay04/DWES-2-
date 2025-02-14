2a4848131c6ecdb8da4ab230fd7948b01b7fb12d
pokeplay04 <149372634+pokeplay04@users.noreply.github.com>
2025-02-14 12:19:04 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,34 @@
 <?php
-
+use App\Http\Controllers\verTareaController;
+use App\Http\Controllers\NuevaControlador;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
 });
 
-Route::get('/form', function(){
-    return view('VistaAlta');
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/newTarea', function(){
+    return view('AñadirTarea');
+});
+
+Route::get('/newClient', function(){
+    return view('AñadirCliente');
 });
 
 Route::get('/inicio', function(){
     return view('menu');
 });
 
+Route::get('/verTareas', function(){
+    return view('verTareas');
+});
+
+Route::resource('tareas', NuevaControlador::class);
+Route::resource('operario', NuevaControlador::class);
 
 
 ?>
